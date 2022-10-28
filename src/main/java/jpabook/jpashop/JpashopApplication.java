9c3364a794f4fb90737b4bf8d0f71e4f01e87c90
suuuuzy <jangsooji91@gmail.com>
2022-10-28 18:40:50 +0900

    간단한 주문 조회 V1 - 엔티티 직접 노출

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
@@ -10,4 +12,15 @@ public class JpashopApplication {
         SpringApplication.run(JpashopApplication.class, args);
     }
 
+    /**
+     * Hibernate5Module
+     * - 프록시 객체를 JSON으로 읽을 수 있도록 도와주는 역할
+     * - 기본적으로 초기화 된 프록시 객체만 노출, 초기화 되지 않은 프록시 객체는 노출 안함
+     */
+    @Bean
+    Hibernate5Module hibernate5Module() {
+        Hibernate5Module hibernate5Module = new Hibernate5Module();
+        //hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+        return hibernate5Module;
+    }
 }
